06eb5f60eba3e0417baefb4f6606b2ce57e38f24
geohasby <geohasby@gmail.com>
2022-06-06 22:46:26 +0700

    update fragment personal and languages

--- app/src/main/java/com/bangkit/dwicara/form/LanguageFragment.kt
+++ app/src/main/java/com/bangkit/dwicara/form/LanguageFragment.kt
@@ -5,24 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bangkit.dwicara.R
 import com.bangkit.dwicara.databinding.FragmentLanguageBinding
 
-class LanguageFragment : Fragment() {
+class LanguageFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentLanguageBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var native: String
+    private lateinit var learn: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentLanguageBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBack.setOnClickListener(this)
+        binding.btnSave.setOnClickListener(this)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_back -> {}
+            R.id.btn_save -> {}
+        }
     }
 }
--- app/src/main/java/com/bangkit/dwicara/form/PersonalFragment.kt
+++ app/src/main/java/com/bangkit/dwicara/form/PersonalFragment.kt
@@ -1,18 +1,32 @@
 package com.bangkit.dwicara.form
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bangkit.dwicara.R
 import com.bangkit.dwicara.databinding.FragmentPersonalBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
-class PersonalFragment : Fragment() {
+class PersonalFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentPersonalBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var username: String
+    private lateinit var fullname: String
+    private lateinit var birthday: String
+    private lateinit var gender: String
 
+    private val cal: Calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +37,104 @@ class PersonalFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener(this)
+        binding.btnNext.setOnClickListener(this)
+
+        val date = DatePickerDialog.OnDateSetListener{ _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel()
+        }
+
+        binding.etBirthday.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                date,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        val genderList = resources.getStringArray(R.array.gender_list)
+        val spinnerAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            genderList
+        )
+
+        binding.etGender.adapter = spinnerAdapter
+
+        binding.etGender.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                gender = genderList[position]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+            }
+    }
+
+    private fun updateLabel() {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        binding.etBirthday.text = sdf.format(cal.time)
+    }
+
+    fun validate(): Boolean {
+        username = binding.etUsername.text.toString()
+        fullname = binding.etFullname.text.toString()
+        birthday = binding.etBirthday.text.toString()
+
+        // Empty Check
+        if (username.isEmpty()) {
+            binding.etUsername.error = getString(R.string.field_empty_error, "username")
+            binding.etUsername.requestFocus()
+            return false
+        }
+
+        if (fullname.isEmpty()) {
+            binding.etFullname.error = getString(R.string.field_empty_error, "full name")
+            binding.etFullname.requestFocus()
+            return false
+        }
+
+//        if(birthday == R.string.enter_your_birthday.toString())
+
+        if (birthday.isEmpty()) {
+            binding.etBirthday.error = getString(R.string.field_empty_error, "birthday")
+            binding.etBirthday.requestFocus()
+            return false
+        }
+
+        return true
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_logout -> {
+                Firebase.auth.signOut()
+                activity?.finish()
+            }
+            R.id.btn_next -> {
+                if(validate()){
+                    findNavController().navigate(R.id.action_personalFragment_to_languageFragment)
+                }
+            }
+        }
     }
 }
