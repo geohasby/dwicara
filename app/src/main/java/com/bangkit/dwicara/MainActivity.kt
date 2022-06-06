51a19873556edf9e56774f291706cdfcb7878cae
Muhammad Faqih Wijaya <mfaqihwijaya3@gmail.com>
2022-06-06 14:45:40 +0700

    Change click to read more text size to 16 sp; Add bottom navigation

--- app/src/main/java/com/bangkit/dwicara/MainActivity.kt
+++ app/src/main/java/com/bangkit/dwicara/MainActivity.kt
@@ -2,7 +2,12 @@ package com.bangkit.dwicara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bangkit.dwicara.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
@@ -10,5 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.container)
+
+        navView.setupWithNavController(navController)
     }
 }
