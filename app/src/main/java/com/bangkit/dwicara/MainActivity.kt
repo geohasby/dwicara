c7a633da3828578e613b1e246aa9df7cdd71f328
Muhammad Faqih Wijaya <mfaqihwijaya3@gmail.com>
2022-06-06 13:36:33 +0700

    Change text content size to 14sp; Finish today's word; Finish today's article;

--- app/src/main/java/com/bangkit/dwicara/MainActivity.kt
+++ app/src/main/java/com/bangkit/dwicara/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bangkit.dwicara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.dwicara.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
